8da3c138f45ae35bf93a1b72d65e304ff0565093
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-03-07 19:34:27 +0800

    one-page abstract

--- build_reflection.sh
+++ build_reflection.sh
@@ -1,4 +1,4 @@
 pandoc --pdf-engine latexmk \
     -V papersize=a4paper -V fontsize=12pt \
     -V geometry:margin=1in -V mainfont=Times \
-    -s narrative_reflection.md -o narrative_reflection.pdf
+    -s narrative_reflection.md -o "sh623_He_2024_Luo_SW EL_Narrative.pdf"
